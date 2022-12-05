6da0744975620632ee02efd4fb19fa762784ccb9
Kauan <114095360+KakaSantosSilva@users.noreply.github.com>
2022-12-05 07:50:27 -0300

    Add files via upload

---
+++ teste03/js/script.js
@@ -0,0 +1,141 @@
+var alb1 = function () {
+    var cor = document.getElementById('yours')
+    cor.style.color = 'red';
+
+
+    var imag = document.getElementsByName('img1')[0]
+    imag.innerText = "Músicas: \n\
+    \n\
+    1.Honeymoon Avenue\n\
+    2.Baby I\n\
+    3.Right There\n\
+    4.Tattooed Heart\n\
+    5.Lovin' It\n\
+    6.Piano\n\
+    7.Daydreamin\n\
+    8.The Way\n\
+    9.You'll Never Know\n\
+    10.Almost Is Never Enough\n\
+    11.Popular Song\n\
+    12.Better Left Unsaid"
+
+};
+
+var alb2 = function () {
+    var cor = document.getElementById('myever')
+    cor.style.color = 'red'
+
+    var image = document.getElementsByName('img2')[0]
+    image.innerText = "Músicas: \n\
+    \n\
+    1.Intro\n\
+    2.Problem(featuring Iggy Azalea)\n\
+    3.One Last Time\n\
+    4.Why Try\n\
+    5.Break Free\n\
+    6.Best Mistake\n\
+    7.Be My Baby\n\
+    8.Break Your Heart Right Back (featuring Childish Gambino)\n\
+    9.Love Me Harder(with the Weeknd)\n\
+    10.Just a Little Bit of Your Heart\n\
+    11.Hands on Me (featuring ASAP Ferg)\n\
+    12.My Everything\n\
+    13.Bang Bang(with Jessie J and Nicki Minaj)\n\
+    14.Only 1\n\
+    15.You Don't Know Me"
+};
+var alb3 = function (){
+    var cor = document.getElementById('dangerous')
+    cor.style.color = 'red'
+
+    var image = document.getElementsByName('img3')[0]
+    image.innerText = "Músicas: \n\
+    \n\
+    1.Moonlight\n\
+    2.Dangerous Woman\n\
+    3.Be Alright\n\
+    4.Into You\n\
+    5.Side to Side (com participação de Nicki Minaj)\n\
+    6.Let Me Love You (com participação de Lil Wayne)\n\
+    7.Greedy\n\
+    8.Leave Me Lonely (com participação de Macy Gray)\n\
+    9.Everyday (com participação de Future)\n\
+    10.Bad Decisions\n\
+    11.Thinking Bout You"
+   
+};
+var alb4 = function (){
+    var cor = document.getElementById('sweet')
+    cor.style.color = 'red'
+
+    var image = document.getElementsByName('img4')[0]
+    image.innerText = "Músicas: \n\
+    \n\
+    1.Raindrops (An Angel Cried)\n\
+    2.Blazed(com participação de Pharrell Williams)\n\
+    3.The Light Is Coming (com participação de Nicki Minaj)\n\
+    4.R.E.M\n\
+    5.God Is a Woman\n\
+    6.Sweetener\n\
+    7.Successful\n\
+    8.Everytime\n\
+    9.Breathin\n\
+    10.No Tears Left to Cry\n\
+    11.Borderline(com participação de Missy Elliott)\n\
+    12.Better Off\n\
+    13.Goodnight n Go\n\
+    14.Pete Davidson\n\
+    15.Get Well Soon"
+   
+};
+var alb5 = function (){
+    var cor = document.getElementById('tun')
+    cor.style.color = 'red'
+
+    var image = document.getElementsByName('img5')[0]
+    image.innerText = "Músicas: \n\
+    \n\
+    1.Imagine\n\
+    2.Needy\n\
+    3.NASA\n\
+    4.Bloodline\n\
+    5.Fake Smile\n\
+    6.Bad Idea\n\
+    7.Make Up\n\
+    8.Ghostin\n\
+    9.In My Head\n\
+    10.7 Rings\n\
+    11.Thank U, Next\n\
+    12.Break Up with Your Girlfriend, I'm Bored"
+    
+   
+};
+var alb6 = function (){
+    var cor = document.getElementById('pst')
+    cor.style.color = 'red'
+
+    var image = document.getElementsByName('img6')[0]
+    image.innerText = "Músicas: \n\
+    \n\
+    1.Shut Up\n\
+    2.34+35\n\
+    3.Motive(com Doja Cat\n\
+    4.Just Like Magic\n\
+    5.Off the Table(com The Weeknd)\n\
+    6.Six Thirty\n\
+    7.Safety Net(com Ty Dolla $ign)\n\
+    8.My Hair\n\
+    9.Nasty\n\
+    10.West Side\n\
+    11.Love Language\n\
+    12.Positions\n\
+    14.Obvious\n\
+    15.POV"
+   
+};
+
+
+
+
+
+
